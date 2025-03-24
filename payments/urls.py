0518db1b6f212cdb7c71774f0fca44babdb5208a
Anyim Ossi <ossifavour@gmail.com>
2025-03-24 17:41:04 +0100

    Update urls.py

--- payments/urls.py
+++ payments/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import InitiatePaymentView, PaymentDetailView, PayPalSuccessView, PayPalCancelView
+from .views import InitiatePaymentView, PaymentDetailView, PayPalSuccessView, PayPalCancelView, PaymentListView
 
 urlpatterns = [
     path('v1/payments/', InitiatePaymentView.as_view(), name='initiate-payment'),
+    path('v1/payments/all/', PaymentListView.as_view(), name='payment-list'),  
     path('v1/payments/<uuid:id>/', PaymentDetailView.as_view(), name='payment-detail'),
     path('v1/payments/paypal/success/', PayPalSuccessView.as_view(), name='paypal-success'),
     path('v1/payments/paypal/cancel/', PayPalCancelView.as_view(), name='paypal-cancel'),
